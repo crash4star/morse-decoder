9db74519b6f46b61911e7dd711e6908864fbd530
crash4star <crashmoney@yandex.ru>
2020-03-21 16:05:29 +0200

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultArr = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        resultArr.push(expr.substring(i, i + 10));
+    };
+
+    return resultArr.map(item => (item.split('**********').join(' ')))
+        .map(item => (+item + ''))
+        .map(item => (item.split('11').join('-')))
+        .map(item => (item.split('10').join('.')))
+        .map(item => (item = MORSE_TABLE[item]))
+        .map(item => item === undefined ? ' ' : item)
+        .join('');
 }
 
 module.exports = {
